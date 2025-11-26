913d14ce62d327488500a00e2161d5e997714d5b
EuFpSilva <filipegsilv7@gmail.com>
2025-11-26 14:10:54 -0300

    Primeiro commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCpE_Zi5JCzCZDhZFWUomiwJ6KV6PBO4Qw",
+    authDomain: "simdigosim-4f0cc.firebaseapp.com",
+    databaseURL: "https://simdigosim-4f0cc-default-rtdb.firebaseio.com",
+    projectId: "simdigosim-4f0cc",
+    storageBucket: "simdigosim-4f0cc.firebasestorage.app",
+    messagingSenderId: "351753097868",
+    appId: "1:351753097868:web:f62fb8ffee73ac48ff61a9",
+    measurementId: "G-3HT6VNZ73C"
+  }
+};
